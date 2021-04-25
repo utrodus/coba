855f22699416463b310f463f4d4f655923e2182e
utrodus <come.programmer@gmail.com>
2021-04-25 21:24:18 +0700

    add response status code

--- server.js
+++ server.js
@@ -13,12 +13,15 @@ const requestListener = (request, response) => {
 
   if (url === '/') {
     if (method === "GET") {
+      response.statusCode = 200;
       response.end("<h1>Ini adalah homepage</h1>")
     } else {
+      response.statusCode = 400;
       response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request!</h1>`);
     }
   } else if (url === '/about') {
     if (method === "GET") {
+      response.statusCode = 200;
       response.end("<h1>Ini adalah halaman about</h1>")
     } else if (method === "POST") {
       let body = [];
@@ -30,12 +33,15 @@ const requestListener = (request, response) => {
       request.on('end', () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
+        response.statusCode = 200;
         response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
       });
     } else {
+      response.statusCode = 400;
       response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request!</h1>`);
     }
   } else {
+    response.statusCode = 404;
     response.end("<h1>Halaman tidak ditemukan!</h1>");
   }
 };
