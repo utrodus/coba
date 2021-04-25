2b0e533e48dec2e26e5490f3a32da683647ebe56
utrodus <come.programmer@gmail.com>
2021-04-25 22:16:35 +0700

    add response body

--- server.js
+++ server.js
@@ -9,21 +9,28 @@ const http = require("http");
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'application/json');
   response.setHeader('X-Powered-By', 'NodeJS');
-  
+
   const { method, url } = request;
 
   if (url === '/') {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end("<h1>Ini adalah homepage</h1>")
+      response.end(JSON.stringify({
+        message: "Ini adalah homepage"
+      }));
     } else {
       response.statusCode = 400;
-      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request!</h1>`);
+      response.end(JSON.stringify({
+        message: `Halaman tidak dapat diakses dengan ${method} request!`
+      }));
+
     }
   } else if (url === '/about') {
     if (method === "GET") {
       response.statusCode = 200;
-      response.end("<h1>Ini adalah halaman about</h1>")
+      response.end(JSON.stringify({
+        message: "Ini adalah halaman about"
+      }));
     } else if (method === "POST") {
       let body = [];
 
@@ -35,15 +42,21 @@ const requestListener = (request, response) => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
         response.statusCode = 200;
-        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+        response.end(JSON.stringify({
+          message: `Halo, ${name}! Ini adalah halaman about`
+        }));
       });
     } else {
       response.statusCode = 400;
-      response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request!</h1>`);
+      response.end(JSON.stringify({
+        message: `Halaman tidak dapat diakses dengan ${method} request!`
+      }));
     }
   } else {
     response.statusCode = 404;
-    response.end("<h1>Halaman tidak ditemukan!</h1>");
+    response.end(JSON.stringify({
+      message: 'Halaman tidak ditemukan!'
+    }));
   }
 };
 
